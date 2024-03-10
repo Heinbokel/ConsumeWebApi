cd95169bbdaaaf67f1fe8502856bfb4bb19673cd
Robert Heinbokel <robertheinbokel@gmail.com>
2024-03-10 19:41:53 -0400

    Now calling for a single TODO and also for multiple TODO's at the same time.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
 ﻿using ConsumeWebApi.models.responses;
 using ConsumeWebApi.services;
 
+// Instantiate our service that calls the external API.
 FreePlaceholderApiService freePlaceholderApiService = new FreePlaceholderApiService();
 
-// Task<Todo?> myTodo = freePlaceholderApiService.RetrieveTodoById(1);
+// Call for a single todo and wait for it to complete.
+Todo? myTodo = await freePlaceholderApiService.RetrieveTodoById(1);
 
-Todo myTodo = await freePlaceholderApiService.RetrieveTodoById(1);
+Console.WriteLine($"TODO ID: {myTodo?.Id} | TITLE: {myTodo?.Title} | COMPLETED: {myTodo?.IsComplete} | USER ID: {myTodo?.UserId}");
 
-// Console.WriteLine($"TITLE: {myTodo?.Title} | COMPLETED: {myTodo?.IsComplete} | USER ID: {myTodo?.UserId} | TODO ID: {myTodo?.Id}");
+// Call for several todo's at the same time.
+Task<Todo?> todoTask1 = freePlaceholderApiService.RetrieveTodoById(2);
+Task<Todo?> todoTask2 = freePlaceholderApiService.RetrieveTodoById(3);
+Task<Todo?> todoTask3 = freePlaceholderApiService.RetrieveTodoById(4);
+Task<Todo?> todoTask4 = freePlaceholderApiService.RetrieveTodoById(5);
+Task<Todo?> todoTask5 = freePlaceholderApiService.RetrieveTodoById(6);
+
+// Store all tasks in a collection.
+List<Task<Todo?>> todoTasks = [todoTask1, todoTask2, todoTask3, todoTask4, todoTask5];
+
+// Await all tasks to complete before continuing.
+await Task.WhenAll(todoTasks);
+
+// Write each todo to the console.
+todoTasks.ForEach(task => {
+    Todo? todo = task.Result;
+    Console.WriteLine($"TODO ID: {todo?.Id} | TITLE: {todo?.Title} | COMPLETED: {todo?.IsComplete} | USER ID: {todo?.UserId}");
+});
+
--- services/FreePlaceholderApiService.cs
+++ services/FreePlaceholderApiService.cs
@@ -7,19 +7,27 @@ namespace ConsumeWebApi.services
 
     public class FreePlaceholderApiService
     {
+        // Store the base URL of the API we are calling so it is easy to reference.
         private static readonly string BASE_URL = "https://jsonplaceholder.typicode.com";
 
+        // Store the HttpClient as a reusable, static field so it uses less resources.
         private static readonly HttpClient _httpClient = new HttpClient();
 
+        /// <summary>
+        /// Retrieves a TODO by its ID from the json placeholder web API.
+        /// </summary>
+        /// <param name="todoId">The ID of the TODO to retrieve.</param>
+        /// <returns>The Task<Todo?> to return.</returns>
+        /// <exception cref="ExternalServiceException">Throws ExternalServiceException if the HTTP Response code is not 200.</exception>
         public async Task<Todo?> RetrieveTodoById(int todoId)
         {
-            Console.WriteLine("CALLING FOR TODO");
+            Console.WriteLine($"CALLING FOR TODO WITH ID {todoId}");
             HttpResponseMessage response = await _httpClient.GetAsync($"{BASE_URL}/todos/{todoId}");
 
             if (response.IsSuccessStatusCode)
             {
                 string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("DONE CALLING FOR TODO");
+                Console.WriteLine($"DONE CALLING FOR TODO WITH ID {todoId}");
                 return JsonSerializer.Deserialize<Todo>(responseBody);
             }
             else
